a3a1fc7b8fed53c8bd190fafc37356424fc26d2e
Olivier Robert <olivierobert@gmail.com>
2022-12-02 13:55:49 +0700

    Add solution for the second part of the first puzzle

--- puzzle-01/solution.rb
+++ puzzle-01/solution.rb
@@ -15,7 +15,11 @@ def find_winning_elf_amount(input)
     elves[elf_index] << line.to_i
   end
 
+  # Find amount of calories of the elves who collected the most stars
   pp elves.map(&:sum).max
+
+  # Find amount of calories of the top 3 elves who collected the most stars
+  pp elves.map(&:sum).sort_by { |elf| -elf }.slice(0, 3).sum
 end
 
 file = File.open("input.txt")
