313a49019f1a80a0d250fe63adadd5dcecaaf4a5
Olivier Robert <olivierobert@gmail.com>
2022-12-06 12:59:39 +0700

    Add solution for the sixth puzzle

---
+++ puzzle-06/solution.rb
@@ -0,0 +1,17 @@
+def find_marker(input, pattern_size)
+  characters_list = input.chars
+
+  characters_list.each_with_index do |character, index|
+    list_to_compare = characters_list.slice((index - (pattern_size - 1))..index)
+
+    if list_to_compare.length == pattern_size && list_to_compare.tally.all? { |key, value| value == 1  }
+      pp index + 1
+      break
+    end
+  end
+end
+
+content = File.read("input.txt")
+
+find_marker(content, 4)
+find_marker(content, 14)
